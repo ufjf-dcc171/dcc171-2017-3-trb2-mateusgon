20c4eac68e2fa4a1a7b011bd423eb94cdb3cecb6
Mateus G <Mateus G@192.168.1.4>
2017-09-22 15:30:44 -0300

    Iniciando

---
+++ src/br/ufjf/dcc171/TrabalhoPizzaria.java
@@ -0,0 +1,10 @@
+package br.ufjf.dcc171;
+
+public class TrabalhoPizzaria {
+
+    public static void main(String[] args) {
+        janelaControle pedidos = new janelaControle();
+        
+    }
+    
+}
